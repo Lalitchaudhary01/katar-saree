8ab06c9271230f9260667c128c469b7e8dc8b6cc
Lalitchaudhary01 <lalitkumar@gmail.com>
2025-06-06 13:33:04 +0530

    add color in prodicts

--- client/src/assets/product/KoraKadhwaStrips.js
+++ client/src/assets/product/KoraKadhwaStrips.js
@@ -15,28 +15,52 @@ const koraKadhwaStripsData = [
       "https://ik.imagekit.io/icrguob6c/katan/IMG_3665.HEIC?updatedAt=1749123186778",
       "https://ik.imagekit.io/icrguob6c/katan/IMG_3666.HEIC?updatedAt=1749123186236",
     ],
-    colors: ["#DAA520", "#F5F5DC", "#C0C0C0"],
+    colors: [
+      "#00BFFF",
+      "#DC143C",
+      "#FFFFFE",
+      "#C0C0C0",
+      "#FFB6C1",
+      "#800080",
+      "#0000FF",
+      "#FFFF00",
+      "#FFFF00",
+      "#000000",
+      "#000080",
+      "#008000",
+    ],
 
     // NEW COLOR-IMAGE MAPPING STRUCTURE
     colorVariants: [
       {
-        color: "#DAA520", // Golden Rod
-        colorName: "Gold",
-        colorCode: "GOLD",
+        color: "#00BFFF", // Aqua blue
+        colorName: "Aqua Blue",
+        colorCode: "AQUA_BLUE",
         images: [
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_2123.HEIC?updatedAt=1749123181527",
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_2114.HEIC?updatedAt=1749123184552",
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_3082.HEIC?updatedAt=1749123175089",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2031.HEIC?updatedAt=1749123185372",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2039.HEIC?updatedAt=1749123185067",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2034.HEIC?updatedAt=1749123175369",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2041.HEIC?updatedAt=1749123179603",
         ],
       },
       {
-        color: "#F5F5DC", // Beige
-        colorName: "Beige",
-        colorCode: "BEIGE",
+        color: "#DC143C", // Dusty Rose Pink
+        colorName: "Dusty Rose Pink",
+        colorCode: "DUSTY_ROSE_PINK",
         images: [
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_2095.HEIC?updatedAt=1749123174751",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2123.HEIC?updatedAt=1749123181527",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2112.HEIC?updatedAt=1749123184942",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2114.HEIC?updatedAt=1749123184552",
+        ],
+      },
+      {
+        color: "#FFFFFE", // Off white
+        colorName: "Off White",
+        colorCode: "OFF_WHITE",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2142.HEIC?updatedAt=1749123184314",
           "https://ik.imagekit.io/icrguob6c/katan/IMG_2147.HEIC?updatedAt=1749123184292",
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_3665.HEIC?updatedAt=1749123186778",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2133.HEIC?updatedAt=1749123181812",
         ],
       },
       {
@@ -44,9 +68,90 @@ const koraKadhwaStripsData = [
         colorName: "Silver",
         colorCode: "SILVER",
         images: [
-          "https://ik.imagekit.io/icrguob6c/katan/IMG_3666.HEIC?updatedAt=1749123186236",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2095.HEIC?updatedAt=1749123174751",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2147.HEIC?updatedAt=1749123184292",
+        ],
+      },
+      {
+        color: "#FFB6C1", // Light pink
+        colorName: "Light Pink",
+        colorCode: "LIGHT_PINK",
+        images: [
           "https://ik.imagekit.io/icrguob6c/katan/IMG_2123.HEIC?updatedAt=1749123181527",
           "https://ik.imagekit.io/icrguob6c/katan/IMG_2114.HEIC?updatedAt=1749123184552",
+        ],
+      },
+      //   {
+      //     color: "#800080", // Maroonish pink
+      //     colorName: "Maroonish Pink",
+      //     colorCode: "MAROONISH_PINK",
+      //     images: [
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_2095.HEIC?updatedAt=1749123174751",
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_2147.HEIC?updatedAt=1749123184292",
+      //     ],
+      //   },
+      {
+        color: "#0000FF", // Cobalt blue color
+        colorName: "Cobalt Blue",
+        colorCode: "COBALT_BLUE",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3659.HEIC?updatedAt=1749123175043",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3658.HEIC?updatedAt=1749123181645",
+        ],
+      },
+      {
+        color: "#FFFF00", // Bright yellow
+        colorName: "Bright Yellow",
+        colorCode: "BRIGHT_YELLOW",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2066.HEIC?updatedAt=1749123179725",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2071.HEIC?updatedAt=1749123179818",
+        ],
+      },
+      {
+        color: "#ADD8E6", // Light blue
+        colorName: "Light Blue",
+        colorCode: "LIGHT_BLUE",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2034.HEIC?updatedAt=1749123175369",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_2033.HEIC?updatedAt=1749123180165",
+        ],
+      },
+      //   {
+      //     color: "#C0C0C0", // Silver color (duplicate but keeping as shown in chat)
+      //     colorName: "Silver Color",
+      //     colorCode: "SILVER_COLOR",
+      //     images: [
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_2147.HEIC?updatedAt=1749123184292",
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_3665.HEIC?updatedAt=1749123186778",
+      //     ],
+      //   },
+
+      {
+        color: "#000000", // Black
+        colorName: "Black",
+        colorCode: "BLACK",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3082.HEIC?updatedAt=1749123175089",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3081.HEIC?updatedAt=1749123175134",
+        ],
+      },
+      //   {
+      //     color: "#000080", // Dark blue
+      //     colorName: "Dark Blue",
+      //     colorCode: "DARK_BLUE",
+      //     images: [
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_2095.HEIC?updatedAt=1749123174751",
+      //       "https://ik.imagekit.io/icrguob6c/katan/IMG_2147.HEIC?updatedAt=1749123184292",
+      //     ],
+      //   },
+      {
+        color: "#008000", // Dark green
+        colorName: "Dark Green",
+        colorCode: "DARK_GREEN",
+        images: [
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3666.HEIC?updatedAt=1749123186236",
+          "https://ik.imagekit.io/icrguob6c/katan/IMG_3665.HEIC?updatedAt=1749123186778",
         ],
       },
     ],
