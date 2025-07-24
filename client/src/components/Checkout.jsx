646403dba94e3b6938910fa7eaa9e88a9e294b72
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-07-24 09:21:19 +0530

    debug a error

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -35,7 +35,7 @@ const Checkout = () => {
   const [error, setError] = useState(null);
 
   const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/";
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
